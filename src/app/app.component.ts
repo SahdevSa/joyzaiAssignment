3f3d78d29f95ef06ffd7ba1cd4928600f7487600
SahdevSa <2019csb1115@iitrpr.ac.in>
2025-10-08 20:20:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+type Tree = { [key: string]: string[] };
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'joyzaiAssign';
+  title = 'joyzaiAssignment';
+
+  tree: Tree = {
+  "a": ["b", "c"],
+  "b": ["d", "e"],
+  "c": ["f", "g"],
+  "e": ["h", "i"],
+  "f": ["j", "k"]
+  };
+
+  flatTree: { name: string; level: number }[] = [];
+
+  constructor() {
+    this.flattenTree('a', 0);
+    this.display();
+  }
+
+  flattenTree(node: string, level: number) {
+    this.flatTree.push({ name: node, level });
+
+    const children = this.tree[node] || [];
+    for (const child of children) {
+      this.flattenTree(child, level + 1);
+    }
+  }
+  
+  display() {
+    console.log(this.flatTree);
+  }
+
 }
